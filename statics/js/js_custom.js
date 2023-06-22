bee02ecb53535cc9298208282cb2628949192d41
tuancuongbota <tuancuong.bota@gmail.com>
2023-06-22 21:47:30 +0700

    cuongnt

--- statics/js/js_custom.js
+++ statics/js/js_custom.js
@@ -9,10 +9,17 @@ $(document).ready(function(){
         autoplaySpeed: 10000,
         customPaging: (_slider, i) => '<div id="dot' + (i + 1) + '"><svg viewBox="0 0 40 40" class="spinner" width="20" height="20" style="--duration: 15.046667s;"><circle cx="20" cy="20" r="12" class="outer" opacity="0.5"></circle><circle cx="20" cy="20" r="5.5" class="inner" opacity="0.5"></circle></svg></div>'
       });
+      var currentVideo;
+      $('.slick-slideshow').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+          if(currentVideo !== undefined){
+              currentVideo.pause();
+          }
+      });
       $('.slick-slideshow').on('afterChange', function(event, slick, currentSlide, nextSlide){
-        var video = $(slick.$slides[currentSlide]).find('.slider-video');
-        if(video.length > 0){
-            video.get(0).play();
-        }
-    });
+          var video = $(slick.$slides[currentSlide]).find('.slider-video');
+          if(video.length > 0){
+              video.get(0).play();
+              currentVideo = video.get(0);
+          }
+      });
 });
